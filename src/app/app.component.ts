850ddaa9b2ece873223316b918273c24b84e64f1
Rysard <rysard.gulbinovic@gmail.com>
2022-02-17 19:40:05 +0200

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Employee} from "./employee";
 import {EmployeeService} from "./employee.service";
 import {HttpErrorResponse} from "@angular/common/http";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -9,10 +10,13 @@ import {HttpErrorResponse} from "@angular/common/http";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  // title!: string; // kad necrushintu appsas (veliau pasiziureti ar nepakeis sio fieldo)
-  public employees!: Employee[];
+  title!: string;
+  public employees: Employee[];
+  public editEmployee: Employee | undefined;
+  public deleteEmployee: Employee | undefined;
 
   constructor(private employeeService: EmployeeService) {
+    this.employees = [];
   }
 
   // overrides OnInit's interface's ngOnInit() method;
@@ -22,8 +26,7 @@ export class AppComponent implements OnInit {
     this.getEmployees();
   }
 
-  // kai kurios .subscribe() implementacijos yra deprecated
-  // pabandyti veliau isimti error dali ir patikrinti ar kodas veiks taip pat
+  // some of the .subscribe() implementations are deprecated
   public getEmployees(): void {
     this.employeeService.getEmployees().subscribe(
       (response: Employee[]) => {
@@ -35,25 +38,82 @@ export class AppComponent implements OnInit {
     );
   }
 
+  // value on the form will be a json representation of all the inputs:
+  public onAddEmployee(addForm: NgForm): void {
+    //@ts-ignore
+    document.getElementById('add-employee-form').click();
+    this.employeeService.addEmployee(addForm.value).subscribe(
+      (response: Employee) => {
+        console.log(response);
+        this.getEmployees();
+        addForm.reset();                // clears the form
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+        addForm.reset();                // clears the form
+      }
+    );
+  }
 
-  public onOpenModal(employee: Employee, mode: string): void {  // 'mode' parameter tells exactly what the user wants to do
+  public onUpdateEmployee(employee: Employee): void {
+    this.employeeService.updateEmployee(employee).subscribe(
+      (response: Employee) => {
+        console.log(response);
+        this.getEmployees();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public onDeleteEmployee(employeeId: number): void {
+    this.employeeService.deleteEmployee(employeeId).subscribe(
+      (response: void) => {
+        console.log(response);
+        this.getEmployees();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public searchEmployees(key: string): void {
+    const results: Employee[] = [];
+    for (const employee of this.employees) {
+      if (employee.name.toLocaleLowerCase().indexOf(key.toLowerCase()) !== -1
+      || employee.email.toLocaleLowerCase().indexOf(key.toLowerCase()) !== -1
+      || employee.phone.toLocaleLowerCase().indexOf(key.toLowerCase()) !== -1
+      || employee.jobTitle.toLocaleLowerCase().indexOf(key.toLowerCase()) !== -1) {
+        results.push(employee);
+      }
+    }
+    this.employees = results;
+    if (results.length === 0 || !key) {
+      this.getEmployees();
+    }
+  }
+
+  public onOpenModal(mode: string, employee?: Employee): void {           // 'mode' parameter tells exactly what the user wants to do
     const container = document.getElementById('main-container'); // gives access to the 'main-container' <div> element in app.component.html
     const button = document.createElement('button');
-    button.type = 'button';                                     // changes the button type from 'submit' to 'button'
+    button.type = 'button';                                               // changes the button type from 'submit' to 'button'
     button.style.display = 'none';
     button.setAttribute('data-toggle', 'modal');
     if (mode === 'add') {
       button.setAttribute('data-target', '#addEmployeeModal');
     }
     if (mode === 'edit') {
+      this.editEmployee = employee;
       button.setAttribute('data-target', '#editEmployeeModal');
     }
     if (mode === 'delete') {
+      this.deleteEmployee = employee;
       button.setAttribute('data-target', '#deleteEmployeeModal');
     }
-    container!.appendChild(button); // added '!' to remove object is possibly null error
+    // @ts-ignore
+    container.appendChild(button);
     button.click();
   }
-
 }
-
